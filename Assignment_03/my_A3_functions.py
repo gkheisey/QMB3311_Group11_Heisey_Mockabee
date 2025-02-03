fd45179549d2cd1ab23373e31f5c4316fdb3e46b
gkheisey <gr897884@ucf.edu>
2025-02-03 18:08:29 -0500

    Update my_A3_functions.py

--- Assignment_03/my_A3_functions.py
+++ Assignment_03/my_A3_functions.py
@@ -40,7 +40,7 @@ import math
 
 # Only function definitions here - no other calculations. 
 
-# Exercise A
+# Exercise 1
 
 def CESutility_valid(good_x: float, good_y: float, parameter: float) -> float:
     """ Returns the same value as CESutility() when x and y are non-negative
@@ -69,6 +69,19 @@ def logit(x, beta0, beta1):
     return math.exp(exponent) / (1 + math.exp(exponent))
 
 
+#Exercise D
+
+
+def logit_like(yi, xi, beta0, beta1):
+    """Computes the log-likelihood of an observation (yi, xi)."""
+    p = logit(xi, beta0, beta1)
+    
+    if yi == 1:
+        return math.log(p)
+    else:
+        return math.log(1 - p)
+
+
 # Only function definitions above this point. 
 
 
@@ -91,6 +104,11 @@ print(CESutility_valid(8, 10, -2))
 print(logit(0, 0, 0))
 print(logit(1, math.log(1), math.log(2)))
 print(logit(2, math.log(1/2), math.log(3)))
+
+#D
+print(logit_like(1, 0, 0, 0))
+print(logit_like(0, 1, 0, 1))
+print(logit_like(1, 2, 1, 1))
 
 ##################################################
 # End
